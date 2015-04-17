3ad9cdb489a43ac5ece03c4ba7b90fcf0a4783e9
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2015-04-17 09:57:56 +0200

    suppress uninteresting build errors

--- uclang_build/build.sh
+++ uclang_build/build.sh
@@ -4,14 +4,14 @@
 python mf_gen.py > Makefile
 
 # - compile libraries -
-make -s -j 3 $(pwd)/tmp_files/libbase_ucll/libbase_ucll.so
-make -s -j 3 $(pwd)/tmp_files/libftgl_ucll/libftgl_ucll.so
-make -s -j 3 $(pwd)/tmp_files/libnode_ucll/libnode_ucll.so
+make -s -j 3 $(pwd)/tmp_files/libbase_ucll/libbase_ucll.so 2> /dev/null
+make -s -j 3 $(pwd)/tmp_files/libftgl_ucll/libftgl_ucll.so 2> /dev/null
+make -s -j 3 $(pwd)/tmp_files/libnode_ucll/libnode_ucll.so 2> /dev/null
 
 # - copy libraries to source -
-cp tmp_files/libbase_ucll/libbase_ucll.so ../uclang/libs/libbase_ucll/
-cp tmp_files/libftgl_ucll/libftgl_ucll.so ../uclang/libs/libftgl_ucll/
-cp tmp_files/libnode_ucll/libnode_ucll.so ../uclang/libs/libnode_ucll/
+cp tmp_files/libbase_ucll/libbase_ucll.so ../uclang/libs/libbase_ucll/ 2> /dev/null
+cp tmp_files/libftgl_ucll/libftgl_ucll.so ../uclang/libs/libftgl_ucll/ 2> /dev/null
+cp tmp_files/libnode_ucll/libnode_ucll.so ../uclang/libs/libnode_ucll/ 2> /dev/null
 
 # - generate makefile with libraries -
 python mf_gen.py > Makefile
