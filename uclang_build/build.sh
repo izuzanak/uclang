fc01b239dcbb0f05605cfe79a8f4076417f38aa8
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2015-05-17 13:34:37 +0200

    unix build script adjusted

--- uclang_build/build.sh
+++ uclang_build/build.sh
@@ -4,27 +4,22 @@
 echo "configure modules"
 python configure.py > mf_mods.py
 
-# - generate makefile -
-echo "generate makefile"
-python mf_gen.py > Makefile
+# - compile base library -
+echo "compile base library"
+python mf_gen.py LIB_BASE > Makefile
+make -s -j 3 $(pwd)/tmp_files/libbase_ucll/libbase_ucll.so 2> /dev/null
+cp tmp_files/libbase_ucll/libbase_ucll.so ../uclang/libs/libbase_ucll/ 2> /dev/null
 
 # - compile libraries -
 echo "compile libraries"
-make -s -j 3 $(pwd)/tmp_files/libbase_ucll/libbase_ucll.so 2> /dev/null
+python mf_gen.py LIB_FTGL LIB_NODE > Makefile
 make -s -j 3 $(pwd)/tmp_files/libftgl_ucll/libftgl_ucll.so 2> /dev/null
 make -s -j 3 $(pwd)/tmp_files/libnode_ucll/libnode_ucll.so 2> /dev/null
-
-# - copy libraries to source -
-echo "copy libraries to source"
-cp tmp_files/libbase_ucll/libbase_ucll.so ../uclang/libs/libbase_ucll/ 2> /dev/null
 cp tmp_files/libftgl_ucll/libftgl_ucll.so ../uclang/libs/libftgl_ucll/ 2> /dev/null
 cp tmp_files/libnode_ucll/libnode_ucll.so ../uclang/libs/libnode_ucll/ 2> /dev/null
 
-# - generate makefile with libraries -
-echo "generate makefile with libraries"
-python mf_gen.py > Makefile
-
-echo "compile executables and modules"
 # - compile executables and modules -
+echo "compile executables and modules"
+python mf_gen.py > Makefile
 make -s -j 3
 
