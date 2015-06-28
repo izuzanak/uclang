a4cfc26392ae8fdd5b9c0a671b5a289cf8b6e7fa
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2015-06-28 07:13:48 +0200

    added uclang try script

---
+++ uclang_try/try_uclang.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Script will clone, compile and test uclang language interpreter. Main purpose
+# of script is to test and demonstrate function of uclang modules: python, lua
+# and v8.
+
+# Usage example:
+#   bash try_uclang.sh
+#   bash try_uclang.sh python lua v8
+
+# PREREQUISITES: bash, git, python2.7, pkg-config, libs used by modules:
+# libv8-dev, libpython2.7-dev, liblua5.2-dev, lua-json, ...
+
+ROOT_DIR=$(pwd)
+
+# - clone repositories -
+if [ ! -d cont ]
+then
+  git clone https://github.com/izuzanak/cont.git
+fi
+
+if [ ! -d uclang ]
+then
+  git clone https://github.com/izuzanak/uclang.git
+fi
+
+# - compile container generator -
+cd cont/cont_build
+sh build.sh
+
+CONT_DIR=$(pwd)
+if [[ ":$PATH:" != *":$CONT_DIR:"* ]]
+then
+    export PATH="${PATH:+"$PATH:"}$CONT_DIR"
+fi
+
+cd $ROOT_DIR
+
+# - compile uclang interpreter -
+cd uclang/uclang_build
+sh build.sh
+
+UCLANG_DIR=$(pwd)
+if [[ ":$LD_LIBRARY_PATH:" != *":$UCLANG_DIR:"* ]]
+then
+    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}$UCLANG_DIR"
+fi
+
+cd $ROOT_DIR
+
+# - run demo scripts -
+UCL_BUILD_DIR=uclang/uclang_build
+
+while true
+do
+  case $1 in
+    python) SCRIPT_NAME=demo_python.ucl ;;
+    lua)    SCRIPT_NAME=demo_lua.ucl ;;
+    v8)     SCRIPT_NAME=demo_v8.ucl ;;
+    *) exit ;;
+  esac
+
+  $UCL_BUILD_DIR/uclang $UCL_BUILD_DIR/scripts/examples/$SCRIPT_NAME
+
+  shift
+done
+
