e553880585fa0abd973355fa827f651bc671a58f
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2017-02-04 15:55:34 +0100

    values conversion from/to prolog terms

--- uclang/mods/prolog_uclm/source_files/ucl_prolog.cc
+++ uclang/mods/prolog_uclm/source_files/ucl_prolog.cc
@@ -41,31 +41,55 @@ bool prolog_c::create_prolog_term(interpreter_thread_s &it,term_t term,location_
   {
     return PL_put_term(term,(term_t)location_ptr->v_data_ptr);
   }
-
-  // FIXME TODO remote dict
-  //else if (location_ptr->v_type != c_rm_class_dict)
-  //{
-  //}
   else
   {
     switch (location_ptr->v_type)
     {
-      // FIXME TODO remove c_bi_class_blank
       case c_bi_class_blank:
-        return PL_put_nil(term);
+        return PL_put_variable(term);
       case c_bi_class_integer:
         return PL_put_integer(term,(long long int)location_ptr->v_data_ptr);
       case c_bi_class_float:
         return PL_put_float(term,(double)location_ptr->v_data_ptr);
       case c_bi_class_string:
-        {
+        {/*{{{*/
           string_s *string_ptr = (string_s *)location_ptr->v_data_ptr;
           atom_t atom = PL_new_atom_nchars(string_ptr->size - 1,string_ptr->data);
           return PL_put_atom(term,atom);
-        }
+        }/*}}}*/
+      case c_bi_class_array:
+        {/*{{{*/
+          pointer_array_s *array_ptr = (pointer_array_s *)location_ptr->v_data_ptr;
 
-      // FIXME TODO array
-      // case c_bi_class_array
+          // - create empty list -
+          PL_put_nil(term);
+
+          if (array_ptr->used != 0)
+          {
+            term_t item_term = PL_new_term_ref();
+
+            // - process array items backwards -
+            pointer *i_ptr = array_ptr->data + array_ptr->used;
+            pointer *i_ptr_end = array_ptr->data;
+            while(--i_ptr >= i_ptr_end)
+            {
+              location_s *item_location = it.get_location_value(*i_ptr);
+
+              if (!prolog_c::create_prolog_term(it,item_term,item_location))
+              {
+                return false;
+              }
+
+              // - append head to tail -
+              if (!PL_cons_list(term,item_term,term))
+              {
+                return false;
+              }
+            }
+          }
+
+          return true;
+        }/*}}}*/
       default:
         return false;
     }
@@ -84,6 +108,8 @@ location_s *prolog_c::prolog_term_value(interpreter_thread_s &it,term_t term,uli
   case PL_ATOM:
     {/*{{{*/
       atom_t atom;
+
+      // - ERROR -
       if (!PL_get_atom(term,&atom))
       {
         return NULL;
@@ -99,12 +125,74 @@ location_s *prolog_c::prolog_term_value(interpreter_thread_s &it,term_t term,uli
       return new_location;
     }/*}}}*/
   case PL_NIL:
+    {/*{{{*/
+      pointer_array_s *array_ptr = it.get_new_array_ptr();
+
+      BIC_CREATE_NEW_LOCATION(new_location,c_bi_class_array,array_ptr);
+      return new_location;
+    }/*}}}*/
+  case PL_INTEGER:
+    {/*{{{*/
+      long long int value;
+
+      // - ERROR -
+      if (!PL_get_int64(term,&value))
+      {
+        return NULL;
+      }
+
+      BIC_CREATE_NEW_LOCATION(new_location,c_bi_class_integer,value);
+      return new_location;
+    }/*}}}*/
+  case PL_FLOAT:
+    {/*{{{*/
+      double value;
+
+      // - ERROR -
+      if (!PL_get_float(term,&value))
+      {
+        return NULL;
+      }
+
+      BIC_CREATE_NEW_LOCATION(new_location,c_bi_class_float,value);
+      return new_location;
+    }/*}}}*/
+  case PL_LIST_PAIR:
+    {/*{{{*/
+      pointer_array_s *array_ptr = it.get_new_array_ptr();
+      BIC_CREATE_NEW_LOCATION(arr_location,c_bi_class_array,array_ptr);
+
+      term_t list = PL_copy_term_ref(term);
+      term_t head = PL_new_term_ref();
+
+      do {
+
+        // - ERROR -
+        if (!PL_get_list(list,head,list))
+        {
+          it.release_location_ptr(arr_location);
+          return NULL;
+        }
+
+        location_s *item_location = prolog_c::prolog_term_value(it,head,source_pos);
+
+        // - ERROR -
+        if (item_location == NULL)
+        {
+          it.release_location_ptr(arr_location);
+          return NULL;
+        }
+
+        // - insert item to array -
+        array_ptr->push(item_location);
+
+      } while(PL_is_pair(list));
+
+      return arr_location;
+    }/*}}}*/
   case PL_BLOB:
   case PL_STRING:
-  case PL_INTEGER:
-  case PL_FLOAT:
   case PL_TERM:
-  case PL_LIST_PAIR:
   case PL_DICT:
   default:
     return NULL;
