db9c83f1a494bd220b25aa5db1d8365ee281644f
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2026-02-01 09:59:45 +0100

    clang-tidy modification

--- uclang/mods/latlon_uclm/source_files/ucl_latlon.cc
+++ uclang/mods/latlon_uclm/source_files/ucl_latlon.cc
@@ -18,7 +18,7 @@ double latlon_s::distance(latlon_s &a_second)
   lat_diff = sin(lat_diff / 2.0);
   lon_diff = sin(lon_diff / 2.0);
 
-  double a = lat_diff*lat_diff + cos(lat1)*cos(lat2)*lon_diff*lon_diff;
+  double a = (lat_diff*lat_diff) + (cos(lat1)*cos(lat2)*lon_diff*lon_diff);
 
   return c_earth_radius_meters*(2.0*atan2(sqrt(a),sqrt(1.0 - a)));
 }/*}}}*/
@@ -31,7 +31,7 @@ double latlon_s::angle(latlon_s &a_second)
   double lon_diff = (a_second.lon - lon)*c_math_pi/180.0;
 
   double y = sin(lon_diff)*cos(lat2);
-  double x = cos(lat1)*sin(lat2) - sin(lat1)*cos(lat2)*cos(lon_diff);
+  double x = (cos(lat1)*sin(lat2)) - (sin(lat1)*cos(lat2)*cos(lon_diff));
 
   double rads = atan2(y,x);
   double degs = rads*180.0/c_math_pi;
